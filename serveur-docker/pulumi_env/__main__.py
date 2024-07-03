1964802a1fa2ab36c3c3a2212f4fdae19c064056
Strandiing <ssstranding@gmail.com>
2024-07-03 11:04:24 +0200

    add bash and pulumli script

---
+++ serveur-docker/pulumi_env/__main__.py
@@ -0,0 +1,19 @@
+import pulumi
+from pulumi_docker import Image, Container, DockerBuildArgs
+
+image_name = "localhost/clamav_cont:v1.0"
+image = Image("clamav-image",
+              build=DockerBuildArgs(context = "/home/gab/scan/user_1/cont_1", platform="linux/amd64"),
+              image_name=image_name,
+              skip_push=True
+)
+
+container = Container("c1_user_x",
+                      image=image.image_name,
+                      name="c1_user_1",
+                      mounts=[{
+                          'target': '/scan/log',
+                          'source': '/home/gab/scan/user_1/malware/test.py_1',
+                          'type': 'bind'
+                      }],
+)
